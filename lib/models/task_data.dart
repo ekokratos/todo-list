4d55370108939c4864f592d28d67938d2cab18d5
ekokratos <adithyams.adi@gmail.com>
2019-10-02 11:14:23 +0530

    Added UI & add task

---
+++ lib/models/task_data.dart
@@ -0,0 +1,26 @@
+import 'dart:collection';
+
+import 'package:flutter/foundation.dart';
+import 'package:todoey/models/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [];
+
+  void addTask(taskName) {
+    _tasks.add(Task(name: taskName));
+    notifyListeners();
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+}
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:todoey/widgets/tasks_list.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/models/task_data.dart';
+
+import 'add_task_screen.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+            context: (context),
+            builder: (context) => AddTaskScreen(),
+          );
+        },
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.toc,
+                    size: 30,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'To-Do List',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  '${Provider.of<TaskData>(context).taskCount} Tasks',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: Container(
+                width: double.infinity,
+                child: Center(
+                  child: Provider.of<TaskData>(context).taskCount == 0
+                      ? Text(
+                          'Add tasks',
+                          style: TextStyle(color: Colors.black54, fontSize: 24),
+                        )
+                      : TasksList(),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
